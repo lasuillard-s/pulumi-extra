b62b5ce2d74c5a229f6d5a0d9130975d497de208
Yuchan Lee <lasuillard@gmail.com>
2025-03-01 14:19:27 +0000

    feat: Inline template support & write tests

--- pulumi_extra/output.py
+++ pulumi_extra/output.py
@@ -2,6 +2,7 @@
 
 from __future__ import annotations
 
+from pathlib import Path
 from typing import TYPE_CHECKING, overload
 
 import pulumi
@@ -9,43 +10,51 @@ from jinja2 import StrictUndefined, Template
 
 if TYPE_CHECKING:
     from collections.abc import Mapping
-    from pathlib import Path
     from typing import Any
 
 
 @overload
 def render_template(
-    template_file: Path,
+    template: Path | str,
     *,
     context: Mapping[str, Any],
-) -> str: ...
+) -> str: ...  # pragma: no cover
 
 
 @overload
 def render_template(
-    template_file: Path,
+    template: Path | str,
     *,
     inputs: Mapping[str, pulumi.Input[Any]],
-) -> pulumi.Output[str]: ...
+) -> pulumi.Output[str]: ...  # pragma: no cover
 
 
 def render_template(
-    template_file: Path,
+    template: Path | str,
     *,
     context: Mapping[str, Any] | None = None,
     inputs: Mapping[str, pulumi.Input[Any]] | None = None,
 ) -> str | pulumi.Output[str]:
-    """Render a template file with the given context."""
-    template = Template(
-        template_file.read_text(),
-        undefined=StrictUndefined,
-    )
+    """Render a template file with the given context.
 
+    Args:
+        template: The template file or inline template string.
+        context: The context to render the template with. Conflicts with inputs.
+        inputs: The inputs to render the template with. Conflicts with context.
+    """
+    if isinstance(template, Path):
+        template = template.read_text()
+
+    jinja_tpl = Template(template, undefined=StrictUndefined)
+
+    # Render with Python values.
     if context is not None and inputs is None:
-        return template.render(context)
+        return jinja_tpl.render(context)
 
+    # Render with Pulumi inputs.
     if context is None and inputs is not None:
-        return pulumi.Output.all(inputs).apply(lambda args: template.render(args[0]))
+        return pulumi.Output.all(inputs).apply(lambda args: jinja_tpl.render(args[0]))
 
-    msg = "Either context or input must be provided." + str(context) + ", " + str(inputs)
+    # Only one of context or inputs must be provided.
+    msg = "Either context or input must be provided."
     raise ValueError(msg)
---
+++ tests/test_output.py
@@ -0,0 +1,82 @@
+from __future__ import annotations
+
+import tempfile
+from pathlib import Path
+from typing import TYPE_CHECKING, Any
+
+import pulumi
+import pytest
+
+from pulumi_extra import render_template
+
+if TYPE_CHECKING:
+    from collections.abc import Iterator
+
+_TEMPLATE = "docker run --detach {{ image }} {{ command }}"
+
+
+@pytest.fixture(
+    params=[
+        {"content": _TEMPLATE, "file": True},
+        {"content": _TEMPLATE, "file": False},
+    ],
+)
+def template(request: pytest.FixtureRequest) -> Iterator[Path | str]:
+    if request.param["file"]:
+        with tempfile.NamedTemporaryFile(mode="w") as tmpfile:
+            file = Path(tmpfile.name)
+            file.write_text("docker run --detach {{ image }} {{ command }}")
+            yield file
+    else:
+        yield _TEMPLATE
+
+
+@pulumi.runtime.test
+def test_render_template_context(template: Path | str) -> Any:
+    """Render template with Python values."""
+    # Arrange
+    image = "busybox:latest"
+    command = "echo 'Hello, World!'"
+
+    # Act
+    result = render_template(template, context={"image": image, "command": command})
+
+    # Assert
+    def check(args: list[Any]) -> None:
+        assert args[0] == "docker run --detach busybox:latest echo 'Hello, World!'"
+
+    return pulumi.Output.all(result).apply(check)
+
+
+@pulumi.runtime.test
+def test_render_template_inputs(template: Path | str) -> Any:
+    """Render template with Pulumi inputs."""
+    # Arrange
+    image = pulumi.Output.from_input("busybox:latest")
+    command = pulumi.Output.from_input("echo 'Hello, World!'")
+
+    # Act
+    result = render_template(
+        template,
+        inputs={"image": image, "command": command},
+    )
+
+    # Assert
+    def check(args: list[Any]) -> None:
+        assert args[0] == "docker run --detach busybox:latest echo 'Hello, World!'"
+
+    return pulumi.Output.all(result).apply(check)
+
+
+def test_render_template_inputs_and_context_mutually_exclusive() -> None:
+    """Render template with Pulumi inputs."""
+    # Arrange
+    # ...
+
+    # Act & Assert
+    with pytest.raises(ValueError, match="Either context or input must be provided."):
+        render_template(  # type: ignore[call-overload]
+            "docker run --detach {{ image }} {{ command }}",
+            context={"don't-care": "yes"},
+            inputs={"don't-care": "yes"},
+        )
