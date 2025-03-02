2d39f68fe8c62a91ef296acd6e66fb91d6b202e5
Yuchan Lee <lasuillard@gmail.com>
2025-03-02 03:53:14 +0000

    refactor: Test code structure

--- tests/test_stack_reference.py
+++ tests/test_stack_reference.py
@@ -10,77 +10,72 @@ from pulumi_extra import get_stack_reference
 from pulumi_extra.stack_reference import get_stack_outputs
 
 
-@pytest.fixture(autouse=True)
-def reset_cache() -> None:
-    """Reset cache for each test."""
-    get_stack_reference.cache_clear()
+class Test__get_stack_reference:
+    @pytest.fixture(autouse=True)
+    def reset_cache(self) -> None:
+        """Reset cache for each test."""
+        get_stack_reference.cache_clear()
+
+    @pytest.mark.parametrize(
+        ("ref", "expect"),
+        [
+            ("dev", "organization/project/dev"),
+            ("network/dev", "organization/network/dev"),
+            ("organization/management/default", "organization/management/default"),
+        ],
+    )
+    @pulumi.runtime.test
+    def test_ref(self, *, ref: str, expect: str) -> Any:
+        # Arrange
+        # ...
+
+        # Act
+        sr = get_stack_reference(ref)
+
+        # Assert
+        def check(args: list[Any]) -> None:
+            sr_name = args[0]
+            assert sr_name == expect
+
+        return pulumi.Output.all(sr.name).apply(check)
+
+    def test_invalid_ref(self) -> Any:
+        # Arrange
+        # ...
+
+        # Act & Assert
+        with pytest.raises(ValueError, match="Invalid stack reference: 'organization/project/dev/extra'"):
+            get_stack_reference("organization/project/dev/extra")
 
 
-@pytest.mark.parametrize(
-    ("ref", "expect"),
-    [
-        ("dev", "organization/project/dev"),
-        ("network/dev", "organization/network/dev"),
-        ("organization/management/default", "organization/management/default"),
-    ],
-)
-@pulumi.runtime.test
-def test_get_stack_reference(*, ref: str, expect: str) -> Any:
-    """Render template with Pulumi inputs."""
-    # Arrange
-    # ...
+class Test__get_stack_outputs:
+    @pytest.mark.parametrize(
+        ("ref", "expect"),
+        [
+            ("dev:ami-id", "ami-id"),
+            ("dev:{ec2-instance-id,elastic-ip}", ["ec2-instance-id", "elastic-ip"]),
+        ],
+    )
+    @pulumi.runtime.test
+    def test_ref(self, *, ref: str, expect: Any) -> Any:
+        # Arrange
+        # ...
 
-    # Act
-    sr = get_stack_reference(ref)
+        # Act
+        with mock.patch("pulumi.StackReference") as m:
+            m.return_value.require_output.side_effect = lambda v: pulumi.Output.from_input(v)
+            outputs = get_stack_outputs(ref)
 
-    # Assert
-    def check(args: list[Any]) -> None:
-        sr_name = args[0]
-        assert sr_name == expect
+        # Assert
+        def check(args: list[Any]) -> None:
+            assert args[0] == expect
 
-    return pulumi.Output.all(sr.name).apply(check)
+        return pulumi.Output.all(outputs).apply(check)
 
+    def test_invalid_ref(self) -> Any:
+        # Arrange
+        # ...
 
-def test_get_stack_reference_invalid_ref() -> Any:
-    """Render template with Pulumi inputs."""
-    # Arrange
-    # ...
-
-    # Act & Assert
-    with pytest.raises(ValueError, match="Invalid stack reference: 'organization/project/dev/extra'"):
-        get_stack_reference("organization/project/dev/extra")
-
-
-@pytest.mark.parametrize(
-    ("ref", "expect"),
-    [
-        ("dev:ami-id", "ami-id"),
-        ("dev:{ec2-instance-id,elastic-ip}", ["ec2-instance-id", "elastic-ip"]),
-    ],
-)
-@pulumi.runtime.test
-def test_get_stack_outputs(*, ref: str, expect: Any) -> Any:
-    """Render template with Pulumi inputs."""
-    # Arrange
-    # ...
-
-    # Act
-    with mock.patch("pulumi.StackReference") as m:
-        m.return_value.require_output.side_effect = lambda v: pulumi.Output.from_input(v)
-        outputs = get_stack_outputs(ref)
-
-    # Assert
-    def check(args: list[Any]) -> None:
-        assert args[0] == expect
-
-    return pulumi.Output.all(outputs).apply(check)
-
-
-def test_get_stack_outputs_invalid_ref() -> Any:
-    """Render template with Pulumi inputs."""
-    # Arrange
-    # ...
-
-    # Act & Assert
-    with pytest.raises(ValueError, match="Invalid output reference: ':output'"):
-        get_stack_outputs(":output")
+        # Act & Assert
+        with pytest.raises(ValueError, match="Invalid output reference: ':output'"):
+            get_stack_outputs(":output")
