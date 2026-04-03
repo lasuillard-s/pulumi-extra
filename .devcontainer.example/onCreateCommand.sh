8fbb968eb39fc33a716ba11c6a725fd61d91cfc7
Yuchan Lee <lasuillard@gmail.com>
2026-04-03 08:28:04 +0000

    chore: update devcontainer configuration

--- .devcontainer.example/onCreateCommand.sh
+++ .devcontainer.example/onCreateCommand.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
+pipx install pre-commit
+pipx install uv
+
 # Install Pulumi CLI
 curl --location --silent --show-error --fail https://get.pulumi.com \
     | sudo sh -s -- --install-root /usr/local
-
-pipx install uv
 
 # Ensure the user's home directory is owned by the user
 sudo chown --recursive "$(id --user):$(id --group)" ~
